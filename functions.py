c5b52b1d3e7c2643dedbd55e26a6d5718e7966a4
Ronel <ronelbautistageronimo@gmail.com>
2025-09-27 17:06:00 +0800

    added changes on functions file

--- functions.py
+++ functions.py
@@ -1,14 +1,32 @@
 person = {"name": "Alice", "age": 30}
 
-def greet(name, age):
-    print(f"Hello {name}, you are {age} years old.")
+# def greet(name, age, /): #<--all parameters should only accept positional arguments
+#     print(f"Hello {name}, you are {age} years old.")
 
-greet(**person)
+# def greet(name, /,age): #<-- name only accepts positional arguments the rest accepts both
+#     print(f"Hello {name}, you are {age} years old.")
+
+# def greet(name, *, age): #<-- age only accepts keyword argument name can be any
+#     print(f"Hello {name}, you are {age} years old.")
+
+# def greet(*,name, age): #<--- all parameters should only accept keyword arguments
+#     print(f"Hello {name}, you are {age} years old.")
+
+# def greet(name, /, *, age):   #<-- name positional only and age keyword only
+#     print(f"Hello {name}, you are {age} years old.")
+
+
+# greet("john",31)
+# greet(age=31, name="Julie")
+# greet("john", age=31)
+# greet(name="john", 31)
+
+# greet(**person)
 
 # -> str is not type check its just a hint to notify user of function that it should return type str
-def ageFunc(age) -> str:
-   return age
-print(ageFunc(person["age"]))
+# def ageFunc(age) -> str:
+#    return age
+# print(ageFunc(person["age"]))
 
 
 # to enforce type checking
@@ -17,4 +35,4 @@ print(ageFunc(person["age"]))
 #         raise TypeError("Expected a string")
 #     return age
 
-print(ageFunc(person["age"]))
+# print(ageFunc(person["age"]))
