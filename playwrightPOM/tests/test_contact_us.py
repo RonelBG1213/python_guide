64e4a67ba736c2ba4abd0d917fa8ab34fc289725
Ronel Geronimo <ronelbautistageronimo@gmail.com>
2025-11-25 12:16:44 +0800

    remove unnecesary page call

--- playwrightPOM/tests/test_contact_us.py
+++ playwrightPOM/tests/test_contact_us.py
@@ -1,9 +1,9 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import expect
 from pages.home_page import StratpointHomePage
 from pages.contact_page import ContactPage
 from pages.navigation import Navigation
 
-def test_navigate_to_contact_us(page: Page):
+def test_navigate_to_contact_us(page):
     home_page = StratpointHomePage(page)
     contact_page = ContactPage(page)
 
@@ -12,7 +12,7 @@ def test_navigate_to_contact_us(page: Page):
     home_page.click_contact_us()
     contact_page.verify_contact_page_loaded()
 
-def test_fill_contact_form(page: Page):
+def test_fill_contact_form(page):
     navigation = Navigation(page)
     contact_page = ContactPage(page)
 
@@ -28,7 +28,7 @@ def test_fill_contact_form(page: Page):
         message="This is a test message"
     )
 
-def test_privacy_notice_link(page: Page):
+def test_privacy_notice_link(page):
     navigation = Navigation(page)
     contact_page = ContactPage(page)
 
@@ -39,7 +39,7 @@ def test_privacy_notice_link(page: Page):
     expect(privacy_page).to_have_url("https://stratpoint.com/privacy-notice/")
     privacy_page.close()
 
-def test_contact_form_fields_visible(page: Page):
+def test_contact_form_fields_visible(page):
     navigation = Navigation(page)
     contact_page = ContactPage(page)
 
--- playwrightPOM/tests/test_homepage.py
+++ playwrightPOM/tests/test_homepage.py
@@ -1,14 +1,13 @@
-from playwright.sync_api import Page
 from pages.home_page import StratpointHomePage
 from pages.navigation import Navigation
 
-def test_homepage_banner_heading(page: Page):
+def test_homepage_banner_heading(page):
     home_page = StratpointHomePage(page)
     home_page.navigate_to_stratpoint()
     home_page.verify_homepage_loaded()
     home_page.verify_banner_heading_visible()
 
-def test_homepage_reload(page: Page):
+def test_homepage_reload(page):
     home_page = StratpointHomePage(page)
     navigation = Navigation(page)
 
